54b551bed2aac140032fcc43cb036baa69306158
coder013 <wnssh607@gmail.com>
2024-03-11 01:21:23 +0900

    프로젝트 생성

---
+++ src/test/java/com/coder013/boardserver/BoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder013.boardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
